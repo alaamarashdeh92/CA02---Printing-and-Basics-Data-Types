9e40cda1ecfcff48609ebb3bb169d9d629c39555
alaamarashdeh92 <9922007303.UPS@htu.edu.jo>
2020-11-06 23:39:36 +0200

    python list index

---
+++ List.py
@@ -0,0 +1,16 @@
+# Python List index
+# *******************#
+# Original list 
+my_list = [54, 44, 27, 79, 91, 41]
+# remove index at (2) 
+new_list = my_list.pop(2)
+# print statement 
+print (f"list= {my_list}")
+# added 27 to index (4) 
+my_list.insert(3,27)
+# print statement 
+#print(my_list)
+# added 27 at the end of the list 
+my_list.append(27)
+# print statement 
+print (f"results_list = {my_list}")
