a8664b47099d281ca01d5e390f3903e6b5448552
alaamarashdeh92 <9922007303.UPS@htu.edu.jo>
2020-11-07 00:58:37 +0200

    Python List max-min

---
+++ list_slice.py
@@ -0,0 +1,24 @@
+# list slice
+#**************#
+# The original list 
+my_list=[1,2,3,4,5,6,7,8,9]
+# First slice list 
+list_1= my_list[:3]
+
+# Second slice list 
+list_2= my_list[3:6]
+
+# Third slice list 
+list_3= my_list[6:]
+
+# The reverce of the first slice list
+list_1.reverse()
+print(list_1)
+
+# The reverce of the Second slice list
+list_2.reverse()
+print(list_2)
+
+# The reverce of the Third slice list
+list_3.reverse()
+print(list_3)
