eecc29a9175ddb3d7db50fd2bf0b895dc38ba3f9
alaamarashdeh92 <9922007303.UPS@htu.edu.jo>
2020-11-07 00:23:45 +0200

    Python List max-min

---
+++ length_list.py
@@ -0,0 +1,6 @@
+# Python List max-min
+# *******************#
+# Original list 
+my_list = [87, 45, 41, 65, 94, 41, 99, 94]
+print("The list contains %d elements." % len(my_list))
+print(f"The max in list is: {max(my_list)} and the min is: {min(my_list)}.")
