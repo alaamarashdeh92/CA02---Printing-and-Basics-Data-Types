325e565947ec1d97f39802c51bbeab17bf83317f
alaamarashdeh92 <9922007303.UPS@htu.edu.jo>
2020-11-07 01:17:13 +0200

    list count word

---
+++ List_count_word.py
@@ -0,0 +1,5 @@
+# list count word 
+# ***************
+my_list = ['Orange', 'Apple', 'Apple', 'Mango', 'Watermelon', 'Apple', 'Strawberry']
+print(my_list.count('Apple'))
+print(my_list.index('Strawberry'))
