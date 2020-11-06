474d6ee66541b96931af15469d8a5dd9a8c68818
alaamarashdeh92 <9922007303.UPS@htu.edu.jo>
2020-11-07 01:05:10 +0200

    list sorted in descending order

---
+++ List_sort.py
@@ -0,0 +1,8 @@
+# List Sort
+# *********#
+# Original list
+my_list = [87, 45, 41, 65, 94, 41, 99, 94]
+my_list.sort()
+my_list.reverse()
+# print the list sorted in descending order
+print(my_list)
